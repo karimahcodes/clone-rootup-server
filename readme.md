c311acb1ed7b35b06e238cb8e41c759b0255a183
Karimah Nichols <ms.karimah.nichols@gmail.com>
2023-06-29 18:05:50 -0400

    update .env variables and PORT

---
+++ readme.md
@@ -0,0 +1,15 @@
+"server-rootup" is the server for the "rootup" repository.
+
+This server runs best on localhost:8080.
+
+The app uses Express and Node and is built in react.
+
+There are no keys required to use the app. 
+
+Dummy farm profiles are saved in farmData.json and corresponding images are stored as static assets in the public / assets / images folder on the server. Both are extracted by an axios call on the front end. 
+
+User data is not currently being used but will be added to future versions of the app.
+
+
+
+For more information or questions: contact rootup.app@gmail.com.
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+require(`dotenv`).config();
 const express = require('express');
 const cors = require("cors");
 const app = express();
 const farmRoutes = require('./routes/farmRoutes');
 const categoriesRoutes = require("./routes/categoriesRoutes");
-// require(`dotenv`).config();
 
 
-// const { PORT, BACKEND_URL} = process.env;
+
+const PORT = process.env.PORT || 8080;
+
 app.use(cors());
 app.use(express.static("public"));
 
@@ -16,6 +18,6 @@ app.use('/categories', categoriesRoutes)
 
 
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
     console.log(`Listening on PORT 8080`);
   });
